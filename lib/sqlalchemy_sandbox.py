2fbcf5552c4941038a59fe4d9199db8a8dfcafd9
Rafael Finol <rafaelfinol88@gmail.com>
2023-08-11 18:36:54 -0400

    lecture complete

--- lib/sqlalchemy_sandbox.py
+++ lib/sqlalchemy_sandbox.py
@@ -1,21 +1,79 @@
 #!/usr/bin/env python3
 
 from datetime import datetime
-
-from sqlalchemy import (create_engine, desc, func,
-    CheckConstraint, PrimaryKeyConstraint, UniqueConstraint,
-    Index, Column, DateTime, Integer, String)
+from sqlalchemy import (
+    create_engine,
+    desc,
+    func,
+    CheckConstraint,
+    PrimaryKeyConstraint,
+    UniqueConstraint,
+    Index,
+    Column,
+    DateTime,
+    Integer,
+    String,
+)
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
 Base = declarative_base()
 
+
 class Student(Base):
-    __tablename__ = 'students'
+    __tablename__ = "students"
+
+    Index("index_name", "name")
 
     id = Column(Integer(), primary_key=True)
     name = Column(String())
+    email = Column(String(55))
+    grade = Column(Integer())
+    birthday = Column(DateTime())
+    enrolled_date = Column(DateTime(), default=datetime.now())
 
-if __name__ == '__main__':
-    engine = create_engine('sqlite:///:memory:')
+    def __repr__(self):
+        return f"Student {self.id}: " + f"{self.name}, " + f"Grade {self.grade}"
+
+
+if __name__ == "__main__":
+    engine = create_engine("sqlite:///:memory:")
     Base.metadata.create_all(engine)
+
+Session = sessionmaker(bind=engine)
+session = Session()
+
+albert_einstein = Student(
+    name="Albert Einstein",
+    email="albert.einstein@zurich.edu",
+    grade=6,
+    birthday=datetime(year=1879, month=3, day=14),
+)
+
+alan_turing = Student(
+    name="Alan Turing",
+    email="alan.turing@sherborne.edu",
+    grade=11,
+    birthday=datetime(year=1912, month=6, day=23),
+)
+
+session.bulk_save_objects([albert_einstein, alan_turing])
+session.commit()
+
+students = session.query(Student)
+names = session.query(Student.name).all()
+students_by_name = session.query(Student.name).order_by(Student.name).all()
+students_by_grade_desc = (
+    session.query(Student.name, Student.grade).order_by(desc(Student.grade)).all()
+)
+
+for student in session.query(Student):
+    student.grade += 1
+
+session.commit()
+
+print([student for student in students])
+print(names)
+print(students_by_name)
+print(students_by_grade_desc)
+print([(student.name, student.grade) for student in session.query(Student)])
